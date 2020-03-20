da9ae05cab523750d05950a2e4141ba4b0e88153
aitorgallardo <gmaitor@outlook.com>
2020-03-20 03:56:59 +0100

    validators on create event form + fixed bug dates

--- src/app/events/event-create/event-create.component.ts
+++ src/app/events/event-create/event-create.component.ts
@@ -1,8 +1,9 @@
 import { EventService } from 'src/app/event.service';
-import { Component, OnInit, ViewChild,ElementRef, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Event } from '../../models/Event';
 import { MapsAPILoader } from '@agm/core';
+import { ThrowStmt } from '@angular/compiler';
 
 
 @Component({
@@ -14,102 +15,106 @@ export class EventCreateComponent implements OnInit, AfterViewInit {
 
   public currentDate = null;
   form: FormGroup;
-
+  submitted = false;
   // map vars
   map: google.maps.Map;
   lat = 41.3851;
   lon = 2.1734;
   coordinates;
   mapOptions: google.maps.MapOptions;
-  marker:google.maps.Marker;
-  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  marker: google.maps.Marker;
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
 
 
-  constructor(private formBuilder: FormBuilder, private eventService: EventService,private mapsAPILoader: MapsAPILoader) {
+  constructor(private formBuilder: FormBuilder, private eventService: EventService, private mapsAPILoader: MapsAPILoader) {
 
     // this service ensures that  Google Maps API is loaded
-    this.mapsAPILoader.load().then(()=>{
+    this.mapsAPILoader.load().then(() => {
       this.coordinates = new google.maps.LatLng(this.lat, this.lon);
-      this.mapOptions= {
+      this.mapOptions = {
         center: this.coordinates,
         zoom: 12,
         disableDefaultUI: true
       };
     });
     this.form = this.formBuilder.group({
-      name: '',
-      nParticipants: '',
-      date: '',
-      time: ''
+      name: ['', Validators.required],
+      nParticipants: ['', []],
+      date: ['', Validators.required],
+      time: ['', Validators.required]
     });
   }
 
   ngOnInit() {
     this.currentDate = new Date().toISOString().slice(0, 10);
-    console.log(this.currentDate);
-    const date = new Date()
   }
 
   ngAfterViewInit() {
     this.mapInitializer();
-   }
+  }
 
-  mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement, 
-    this.mapOptions);
-    this.map.addListener("click", (event) => {
-      console.log('map info',event.latLng);
-  this.setMarker(event.latLng)
-
-    });
-   }
+  get f() { return this.form.controls; }
 
   onSubmit() {
+
+    this.submitted = true;
+
     console.warn('Your order has been submitted', this.form.value);
-    const event = new Event();
-    event.name = this.form.get('name').value;
-    event.maxMembers = this.form.get('nParticipants').value;
-    const date = this.form.get('date').value;
-    const time = this.form.get('time').value;
-    event.date = this.generateDate(date, time);
-    event.latitude = 43;
-    event.longitude = 2;
 
-    this.eventService.createEvent(event).subscribe(res=>{
-      console.log('resultado del form',res)
-    });
+    if (this.form.valid && this.marker) {
+      const event = new Event();
+      event.name = this.form.get('name').value;
+      event.maxMembers = this.form.get('nParticipants').value;
+      const date = this.form.get('date').value;
+      const time = this.form.get('time').value;
+      event.date = this.generateDate(date, time);
+      event.latitude = this.marker.getPosition().lat();
+      event.longitude = this.marker.getPosition().lng();
 
+      this.eventService.createEvent(event).subscribe(res => {
+        this.onReset();
+      });
+    } else {
 
+    }
+  }
+
+  onReset() {
+    this.submitted = false;
     this.form.reset();
   }
 
   generateDate(date: string, time: string) {
     const dateToArray: string[] = date.split('-');
     const year = parseInt(dateToArray[0]);
-    console.log(year);
-    const month = parseInt(dateToArray[1]);
-    console.log(month);
+    const month = parseInt(dateToArray[1])-1; // values between 0-11
     const day = parseInt(dateToArray[2]);
-    console.log(day);
+
     const timeToArray: string[] = time.split(':');
-    console.log(timeToArray);
-    
+
     const hours = parseInt(timeToArray[0]);
-    console.log(hours);
     const minutes = parseInt(timeToArray[1]);
-    console.log(minutes);
 
     return new Date(Date.UTC(year, month, day, hours, minutes));
   }
 
-  setMarker(latlng){
-    if(this.marker){
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+      this.mapOptions);
+    this.map.addListener("click", (event) => {
+      this.setMarker(event.latLng)
+
+    });
+  }
+
+  setMarker(latlng) {
+    if (this.marker) {
       this.marker.setMap(null);
     }
     this.marker = new google.maps.Marker({
       position: latlng,
-      map: this.map 
-      
+      map: this.map
+
     });
     this.marker.setMap(this.map);
   }
